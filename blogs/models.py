cf76c1ed7edd583cb2e511cd93c85229ea267a40
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-02-28 00:22:19 +0900

    change for admin

--- blogs/models.py
+++ blogs/models.py
@@ -5,9 +5,14 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     user = models.ForeignKey(User)
     title = models.CharField(max_length=200)
     text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
